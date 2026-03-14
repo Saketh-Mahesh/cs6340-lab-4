faca8f4256655bbc7da36b08d565fe91df0ed722
Saketh-Mahesh <81051119+Saketh-Mahesh@users.noreply.github.com>
2026-03-14 03:36:45 +0000

    part 3

--- typescript/FractionMatcher/LevelNode.ts
+++ typescript/FractionMatcher/LevelNode.ts
@@ -1,5 +1,17 @@
 "use strict";
-export function dropShapeToZone(shape, zoneIndex) {
+
+interface Shape {
+    numerator: number;
+    denominator: number;
+    dropZone: number;
+    view?: {
+        indexShape: number;
+        height: number;
+        moveToFront: () => void;
+    }
+}
+
+export function dropShapeToZone(shape: Shape, zoneIndex: number) {
     var model = {
         gameModel: {
             MINIMUM_PAIRS: 1,
@@ -8,14 +20,18 @@ export function dropShapeToZone(shape, zoneIndex) {
         dropZone: [1, 2]
     };
     //target dropZone now equals the indexShape
-    model.dropZone[zoneIndex] = shape.view.indexShape;
+    if (shape.view) {
+        model.dropZone[zoneIndex] = shape.view.indexShape;
+    }
     shape.dropZone = zoneIndex;
     var targetPosition = {
         x: 100,
         y: 100
     };
     if (zoneIndex > model.gameModel.MAXIMUM_PAIRS * 2 - 1) {
-        targetPosition.y -= shape.view.height / 2 - 13; //adjust position on scales
+        if (shape.view) {
+            targetPosition.y -= shape.view.height / 2 - 13; //adjust position on scales
+        }
         //Adjust numeric mixed fractions down a bit because they are too high by default.
         if (shape.view && shape.numerator / shape.denominator > 1) {
             targetPosition.y += 11;
@@ -24,5 +40,7 @@ export function dropShapeToZone(shape, zoneIndex) {
             targetPosition.y += 7;
         }
     }
-    shape.view.moveToFront();
+    if (shape.view) {
+        shape.view.moveToFront();
+    }
 }
